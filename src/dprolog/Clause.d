4c1649d8cb9f02d8d0957f1907e8922dad20e05b
Ark <target.ark@gmail.com>
2017-03-28 20:46:05 +0900

    add: Clause

---
+++ src/dprolog/Clause.d
@@ -0,0 +1,55 @@
+module dprolog.Clause;
+
+import dprolog.Token,
+       dprolog.Term;
+
+import std.conv;
+
+interface Clause {}
+
+class Fact : Clause {
+    private Term term;
+
+    this(Term term) {
+        this.term = term;
+    }
+
+    override string toString() {
+        return "Fact(" ~ term.to!string ~ ")";
+    }
+
+    invariant {
+        assert(term.isDetermined);
+        assert(!term.isCompound);
+    }
+}
+
+class Rule : Clause {
+    private Term headTerm;
+    private Term bodyCompoundTerm;
+
+    this(Term headTerm, Term bodyCompoundTerm) {
+        this.headTerm  = headTerm;
+        this.bodyCompoundTerm = bodyCompoundTerm;
+    }
+
+    override string toString() {
+        return "Rule(" ~ headTerm.to!string ~ " :- " ~ bodyCompoundTerm.to!string ~ ")";
+    }
+
+    invariant {
+        assert(!headTerm.isCompound);
+    }
+}
+
+class Query : Clause {
+    private Term compoundTerm;
+
+    this(Term compoundTerm) {
+        this.compoundTerm = compoundTerm;
+    }
+
+    override string toString() {
+        return "Query(?- " ~ compoundTerm.to!string ~ ")";
+    }
+}
