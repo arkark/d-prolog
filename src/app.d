187c17fbd69f33415e9c55505ec9754af325eaf1
Ark <target.ark@gmail.com>
2018-10-01 02:32:04 +0900

    [add] Add help option

--- src/app.d
+++ src/app.d
@@ -1,19 +1,40 @@
 
 import dprolog.engine.Engine;
 
+import std.stdio;
+import std.format;
 import std.conv;
 import std.string;
+import std.algorithm;
 import std.getopt;
+import std.typecons;
+
+enum fileOption = Option("f", "file", "Read `VALUE` as a user initialization file", true);
+enum verboseOption = Option("v", "verbose", "Print diagnostic output", false);
+enum helpOption = Option("h", "help", "This help information", false);
 
 void main(string[] args) {
 
   string filePath;
   bool verbose;
-  auto opt = getopt(
-    args,
-    "file|f", &filePath,
-    "verbose|v", &verbose
-  );
+  bool help;
+
+  try {
+    args.getopt(
+      fileOption.params(filePath).expand,
+      verboseOption.params(verbose).expand,
+      helpOption.params(help).expand
+    );
+  } catch(Throwable e) {
+    writeln(format!"Error processing arguments: %s"(e.msg));
+    writeln("Run 'dprolog --help' for usage information.");
+    return;
+  }
+
+  if (help) {
+    printUsage!([fileOption, verboseOption, helpOption]);
+    return;
+  }
 
   Engine engine = new Engine;
   engine.verboseMode = verbose;
@@ -28,4 +49,38 @@ void main(string[] args) {
     engine.next();
   }
   destroy(engine);
+}
+
+struct Option {
+  string shortOpt;
+  string longOpt;
+  string description;
+  bool requiredValue;
+  auto params(T)(ref T value) {
+    return tuple(format!"%s|%s"(longOpt, shortOpt), description, &value);
+  }
+  string shortString() @property {
+    return format!"-%s"(shortOpt);
+  }
+  string longString() @property {
+    return format!"--%s%s"(longOpt, requiredValue ? "=VALUE" : "");
+  }
+}
+
+void printUsage(Option[] options)() {
+  writeln("Usage:");
+  writeln("    dprolog [<options...>]");
+  writeln;
+  writeln("Options:");
+  enum len1 = options.map!"a.shortString.length".reduce!max.to!string;
+  enum len2 = options.map!"a.longString.length".reduce!max.to!string;
+  foreach(opt; options) {
+    format!(
+      "    %"~ len1 ~"s  %"~ len2 ~"s  %s"
+    )(
+      opt.shortString,
+      opt.longString,
+      opt.description
+    ).writeln;
+  }
 }
