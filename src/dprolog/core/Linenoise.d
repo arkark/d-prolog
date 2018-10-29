84ac9ccc9c7b7d21a129e40c53b9b4d94739de4b
Ark <target.ark@gmail.com>
2018-10-29 15:51:17 +0900

    [add] Wrap Linenoise functions

---
+++ src/dprolog/core/Linenoise.d
@@ -0,0 +1,69 @@
+module dprolog.core.Linenoise;
+
+import dprolog.util.Maybe;
+
+import std.conv;
+import std.string;
+
+import raw = deimos.linenoise;
+
+@property Linenoise_ Linenoise() {
+  static Linenoise_ instance;
+  if (!instance) {
+    instance = new Linenoise_();
+  }
+  return instance;
+}
+
+private class Linenoise_ {
+  private enum defaultMaxHistoryLength = 100;
+
+  this() {
+    setMaxHistoryLength(defaultMaxHistoryLength);
+  }
+
+  Maybe!string nextLine(string prompt) {
+    char* line = raw.linenoise(prompt.toStringz);
+    return (line !is null).fmap!({
+      string result = line.fromStringz.to!string;
+      destroy(line);
+      return result;
+    });
+  }
+
+  bool addHistory(string line) {
+    return raw.linenoiseHistoryAdd(line.toStringz) != 0;
+  }
+
+  bool setMaxHistoryLength(size_t length) {
+    return raw.linenoiseHistorySetMaxLen(length.to!int) != 0;
+  }
+
+  bool saveHistory(string fileName) {
+    return raw.linenoiseHistorySave(fileName.toStringz) != 0;
+  }
+
+  bool loadHistory(string fileName) {
+    return raw.linenoiseHistoryLoad(fileName.toStringz) != 0;
+  }
+
+  void clearScreen() {
+    raw.linenoiseClearScreen();
+  }
+
+  void enableMultiLineMode() {
+    raw.linenoiseSetMultiLine(1);
+  }
+
+  void disableMultiLineMode() {
+    raw.linenoiseSetMultiLine(0);
+  }
+
+  /*
+    The followings aren't wrapped.
+    - struct linenoiseCompletions
+    - alias linenoiseCompletionCallback
+    - void linenoiseSetCompletionCallback(linenoiseCompletionCallback)
+    - void linenoiseAddCompletion(linenoiseCompletions *, const char *)
+   */
+}
