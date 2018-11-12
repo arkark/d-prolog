8e50b6658585576fff6a6f6a22b22e8951563057
Ark <target.ark@gmail.com>
2018-11-13 01:54:10 +0900

    [update] Refactor Tty.d

--- src/dprolog/core/Tty.d
+++ src/dprolog/core/Tty.d
@@ -1,28 +1,23 @@
 module dprolog.core.Tty;
 
-import core.sys.posix.sys.ioctl;
-import std.stdio;
-
 import dprolog.util.Either;
 import dprolog.util.Message;
 
-Either!(Message, int) getColumns()
-{
-    auto size = winsize();
-    if (ioctl(stdout.fileno(), TIOCGWINSZ, &size) == -1)
-    {
-        return ErrorMessage("Cannot get column size").Left!(Message, int);
-    }
-    return Right!(Message, int)(size.ws_col);
+import core.sys.posix.sys.ioctl;
+import std.stdio;
 
+Either!(Message, int) getColumns() {
+  auto size = winsize();
+  if (ioctl(stdout.fileno(), TIOCGWINSZ, &size) == -1) {
+    return ErrorMessage("Cannot get column size").Left!(Message, int);
+  }
+  return Right!(Message, int)(size.ws_col);
 }
 
-Either!(Message, int) getRows()
-{
-    auto size = winsize();
-    if (ioctl(stdout.fileno(), TIOCGWINSZ, &size) == -1)
-    {
-        return ErrorMessage("Cannot get row size").Left!(Message, int);
-    }
-    return Right!(Message, int)(size.ws_row);
+Either!(Message, int) getRows() {
+  auto size = winsize();
+  if (ioctl(stdout.fileno(), TIOCGWINSZ, &size) == -1) {
+    return ErrorMessage("Cannot get row size").Left!(Message, int);
+  }
+  return Right!(Message, int)(size.ws_row);
 }
