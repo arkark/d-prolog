73182300814a2d29a88d8cf96e107fdd87408181
Ark <target.ark@gmail.com>
2018-11-20 20:38:19 +0900

    [add] Add --force option to release build

--- release.sh
+++ release.sh
@@ -17,7 +17,7 @@ FILE_NAME="dprolog-$VERSION-$OS-$ARCH"
 echo "Building $FILE_NAME"
 
 mkdir "bin/$FILE_NAME"
-dub build -b release --arch=x86_64
+dub build -b release --arch=x86_64 --force
 cp "bin/dprolog" "bin/$FILE_NAME"
 cp -r example "bin/$FILE_NAME/example"
 
