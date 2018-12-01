b417af554887160080b21d9f8edcce81ba07f20e
Ark <target.ark@gmail.com>
2018-12-01 11:00:37 +0900

    [update] Refactor Shell.d

--- src/dprolog/core/Shell.d
+++ src/dprolog/core/Shell.d
@@ -9,56 +9,59 @@ import std.conv;
 import std.string;
 import std.process;
 import std.file : chdir, FileException;
+import std.traits;
 
 alias Shell = Singleton!Shell_;
 
 class Shell_ {
 
+private:
+  alias Result = ReturnType!executeShell;
+
+public:
   Either!(Message, string[]) executeLs() {
-    try {
-      auto result = executeShell("ls");
+    return execute("ls").bind!((result) {
       if (result.status == 0) {
         return result.output.chomp.splitLines.Right!(Message, string[]);
       } else {
         return ErrorMessage(result.output.chomp).Left!(Message, string[]);
       }
-    } catch (ProcessException e) {
-      return ErrorMessage(e.msg).Left!(Message, string[]);
-    } catch (StdioException e) {
-      return ErrorMessage(e.msg).Left!(Message, string[]);
-    }
+    });
   }
 
   Either!(Message, string[]) executeLsWithPath(string path) {
-    try {
-      auto result = executeShell("ls " ~ path);
+    return execute("ls " ~ path).bind!((result) {
       if (result.status == 0) {
         return result.output.chomp.splitLines.Right!(Message, string[]);
       } else {
         return ErrorMessage(result.output.chomp).Left!(Message, string[]);
       }
-    } catch (ProcessException e) {
-      return ErrorMessage(e.msg).Left!(Message, string[]);
-    } catch (StdioException e) {
-      return ErrorMessage(e.msg).Left!(Message, string[]);
-    }
+    });
   }
 
   Either!(Message, string[]) executeCdWithPath(string path) {
-    try {
-      auto echoResult = executeShell("echo " ~ path);
-      if (echoResult.status == 0) {
-        echoResult.output.chomp.chdir;
-      } else {
-        path.chdir;
+    return execute("echo " ~ path).bind!((result) {
+      try {
+        if (result.status == 0) {
+          result.output.chomp.chdir;
+        } else {
+          path.chdir;
+        }
+        return [].Right!(Message, string[]);
+      } catch (FileException e) {
+        return ErrorMessage(e.msg).Left!(Message, string[]);
       }
-      return [].Right!(Message, string[]);
+    });
+  }
+
+private:
+  Either!(Message, Result) execute(string command) {
+    try {
+      return executeShell(command).Right!(Message, Result);
     } catch (ProcessException e) {
-      return ErrorMessage(e.msg).Left!(Message, string[]);
+      return ErrorMessage(e.msg).Left!(Message, Result);
     } catch (StdioException e) {
-      return ErrorMessage(e.msg).Left!(Message, string[]);
-    } catch (FileException e) {
-      return ErrorMessage(e.msg).Left!(Message, string[]);
+      return ErrorMessage(e.msg).Left!(Message, Result);
     }
   }
 }
